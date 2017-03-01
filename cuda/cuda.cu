5295544f172e2399031ba1972ab1f0066b92256d
Greg Gaston <ggaston@adelie.cs.pdx.edu>
2017-03-01 15:07:38 -0800

    added liscensing Inforamtion

--- cuda/cuda.cu
+++ cuda/cuda.cu
@@ -1,3 +1,21 @@
+/*
+  Copyright (c) 2017 
+  Gregory Gaston, Geoffrey Maggi, Prajyoth Bhandary, and Sriharsha Makineni
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda.h>
@@ -112,11 +130,3 @@ int main(int argc, char *argv[]) {
 
   return 0;
 }
-
-/*
-4 / n 
-
-2 * n + 0
-0 1 2 3 4 5 6 7 8
-
-*/
