da611ba7d46df65d16722e1b0f3b23cfa80501e9
Greg <ggaston@pdx.edu>
2017-03-02 23:08:31 -0800

    JSON is too fancy for what we need...

--- data/gendata.py
+++ data/gendata.py
@@ -1,23 +1,26 @@
 import sys
-import json
 import random
 
 def main(n, m, filename):
   random.seed()
   #Generate random numbers where 0 <= num < 10000
   data = [[random.random() * 10 ** random.randint(0, 4) for j in range(0, m)] for i in range(0, n)]
-
-  #Write to JSON File
-  with open(filename, "w") as file:
-    json.dump({'data':data}, file, indent=2)  
+  
+  #Write file
+  file = open(filename, 'w')
+  file.write(str(n)); file.write("\n")
+  file.write(str(m)); file.write("\n")
+  for row in data:
+    for num in row:
+      file.write(str(num)); file.write("\n")
   file.close()
 
   print(n, "x", m, " file generaged as '", filename, "'", sep = '')
 
 if __name__ == "__main__":
   if len(sys.argv) == 2:
-    main(int(sys.argv[1]), int(sys.argv[1]), sys.argv[1] + 'x' + sys.argv[1] + '.json')
+    main(int(sys.argv[1]), int(sys.argv[1]), sys.argv[1] + 'x' + sys.argv[1] + '.txt')
   elif len(sys.argv) == 3:
-    main(int(sys.argv[1]), int(sys.argv[2]), sys.argv[1] + 'x' + sys.argv[2] + '.json')
+    main(int(sys.argv[1]), int(sys.argv[2]), sys.argv[1] + 'x' + sys.argv[2] + '.txt')
   else:
     print("Error: Called with 1 or 2 integer arguments")
