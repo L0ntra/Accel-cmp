92b10f679a686cd723785aa9c1336842447413c3
Greg <ggaston@pdx.edu>
2017-03-04 11:35:36 -0800

    Gen Data optimization - Write direcly to the file instead of storing in var

--- data/gendata.py
+++ data/gendata.py
@@ -4,15 +4,18 @@ import random
 
 def main(n, m, filename):
   random.seed()
-  #Generate random numbers where 0 <= num < 10000
-  data = [[random.random() * 10 ** random.randint(0, 4) for j in range(0, m)] for i in range(0, n)]
 
-  #Write to JSON File
-  with open(filename, "w") as file:
-    json.dump({'data':data}, file, indent=2)  
+  file = open(filename, 'w')
+  file.write(str(n)); file.write("\n")
+  file.write(str(m)); file.write("\n")
+  for i in range(0, m):
+    for j in range(0, n):
+      file.write(str(random.random() * 10 ** random.randint(0, 4)))
+      file.write("\n")
+
   file.close()
 
-  print(n, "x", m, " file generaged as '", filename, "'", sep = '')
+  print(m, "x", n, " file generaged as '", filename, "'", sep = '')
 
 if __name__ == "__main__":
   if len(sys.argv) == 2:
